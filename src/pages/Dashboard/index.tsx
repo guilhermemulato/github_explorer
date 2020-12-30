77872ade95f6f190a7d2dae51e74679604ada9a0
Guilherme Mulato <guilherme_mulato@hotmail.com>
2020-12-30 14:01:36 -0300

    Finalizado aplicacao

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,94 @@
+import React, { useState, FormEvent, useEffect } from 'react';
+
+import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.svg';
+import api from '../../services/api';
+
+import { Title, Form, Repositories, Error } from './styles';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
+const Dashboard: React.FC = () => {
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+    const [repositories, setRepositories] = useState<Repository[]>(() => {
+        const storage = localStorage.getItem('@GithubExpĺorer:repositories');
+
+        if (storage) {
+            return JSON.parse(storage);
+        }
+        return [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem(
+            '@GithubExpĺorer:repositories',
+            JSON.stringify(repositories),
+        );
+    }, [repositories]);
+
+    async function handleAddRepository(event: FormEvent): Promise<void> {
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+
+        try {
+            const response = await api.get(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...repositories, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (err) {
+            setInputError('Erro na busca por esse repositorio');
+        }
+    }
+
+    return (
+        <>
+            <img src={logo} alt="github_explorer" />
+            <Title>Explore repositórios no Github</Title>
+            <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+                <input
+                    placeholder="Digite o nome do repositório"
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                />
+                <button type="submit">Pesquisar</button>
+            </Form>
+            {inputError && <Error>{inputError}</Error>}
+            <Repositories>
+                {repositories.map((repository) => (
+                    <Link
+                        key={repository.full_name}
+                        to={`/repositories/${repository.full_name}`}
+                    >
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </Link>
+                ))}
+            </Repositories>
+        </>
+    );
+};
+
+export default Dashboard;
